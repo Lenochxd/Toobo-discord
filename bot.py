9451653164fc94e37a7773ad2b852a641d44ae02
Lenoch <81lennoch@gmail.com>
2024-07-24 19:08:19 +0200

    Add daily meteo message based on user config

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import json
 import nextcord
-from nextcord.ext import commands
+from datetime import datetime
+from nextcord.ext import commands, tasks
 
 from handlers.message_handler import handle_message
 from handlers.slash_commands import register_slash_commands
 
+from commands.meteo import get_meteo
+from utils.settings import daily
 from utils.languages import init as langs_init
 import utils.sql as db
 
@@ -23,10 +26,6 @@ config = load_config()
 
 bot = commands.Bot(command_prefix=config['default-prefix'], intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-
 
 @bot.event
 async def on_message(message):
@@ -34,6 +33,28 @@ async def on_message(message):
     await handle_message(bot, message)
 
 register_slash_commands(bot)
+
+
+@tasks.loop(minutes=1)
+async def minute_loop():
+    for guild in bot.guilds:
+        time = daily.get_time(guild.id)
+        
+        if time and time == datetime.now().strftime("%H:%M"):
+            channel_id = daily.get_channel(guild.id)
+            channel = bot.get_channel(channel_id)
+            if channel:
+                await channel.send(
+                    await get_meteo(
+                        config.get('default-language', 'fr')
+                    )
+                )
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    minute_loop.start()
+
 
 if config.get('production', False) == True:
     bot.run(config['tokens']['main'])
