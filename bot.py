666ac351f420886e51c89f801556c92bc7e62354
Lenoch <81lennoch@gmail.com>
2024-08-08 17:16:31 +0200

    Improve error handling for daily weather messages
    - Added error handling in `minute_loop` to manage cases where sending messages to a channel fails:
      - If sending fails due to permission issues (`Forbidden`), attempts to DM the guild owner with an error message.
      - If the channel is not found (`NotFound`), attempts to DM the guild owner with a different error message.
      - Gracefully handles potential failures in DMing the guild owner (`HTTPException`).
    - Updated the French translation file `fr_FR.lang` with new error messages for daily weather notifications:
      - `meteo_daily_403_error`: Error message for permission issues.
      - `meteo_daily_404_error`: Error message for missing channels.

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import json
 import nextcord
 from datetime import datetime
 from nextcord.ext import commands, tasks
@@ -9,6 +8,7 @@ from handlers.slash_commands import register_slash_commands
 from commands.meteo import get_meteo
 from utils.settings import daily
 from utils.languages import init as langs_init
+from utils.languages import text
 from utils.config import config
 import utils.sql as db
 
@@ -35,6 +35,7 @@ register_slash_commands(bot)
 
 @tasks.loop(minutes=1)
 async def minute_loop():
+    default_language = config.get('default-language', 'fr')
     for guild in bot.guilds:
         time = daily.get_time(guild.id)
         
@@ -42,11 +43,36 @@ async def minute_loop():
             channel_id = daily.get_channel(guild.id)
             channel = bot.get_channel(channel_id)
             if channel and daily.is_enabled(guild.id) == True:
-                await channel.send(
-                    await get_meteo(
-                        config.get('default-language', 'fr')
+                try:
+                    await channel.send(
+                        await get_meteo(default_language)
                     )
-                )
+                    
+                except nextcord.errors.Forbidden:
+                    # If sending to the channel fails, try to DM the guild owner
+                    try:
+                        guild_owner = guild.owner
+                        if guild_owner:
+                            await guild_owner.send(
+                                text('meteo_daily_403_error', default_language).replace('%channel_mention%', channel.mention)
+                            )
+                    except nextcord.errors.HTTPException:
+                        # If DMing also fails, we can't do much more
+                        pass
+                    
+                except nextcord.errors.NotFound:
+                    # If the channel doesn't exist, try to DM the guild owner
+                    try:
+                        guild_owner = guild.owner
+                        if guild_owner:
+                            await guild_owner.send(
+                                text('meteo_daily_404_error', default_language)
+                            )
+                    except nextcord.errors.HTTPException:
+                        # If DMing also fails, we can't do much more
+                        pass
+
+
 
 @bot.event
 async def on_ready():
